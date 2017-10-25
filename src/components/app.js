fd8ab2f2b296757237ed3c00914fb4b55ff65177
ergot <wjose14@hotmail.com>
2017-10-25 12:02:22 +0200

    4-41

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from '../containers/search-bar'
 
+const DEFAULT_COUNTRY="France"
+
 export default class App extends React.Component {
   render() {
     return (
-  <div><SearchBar/></div>
+  <div><SearchBar defaultCountry={DEFAULT_COUNTRY}/></div>
     );
   }
 }
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,13 +4,43 @@ import {bindActionCreators} from 'redux'
 import {getCountries} from "../actions/index";
 
 class SearchBar extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {selectedCountry : this.props.defaultCountry}
+    }
+
+
     componentWillMount () {
         this.props.getCountries()
     }
 
+    search(e) {
+        this.setState({selectedCountry:target.value})
+    }
+
+    renderSelectBox (){
+        const {countries} = this.props
+        if(countries) {
+            return (
+                <select className="col-lg-12 input-group" value={this.state.selectedCountry} onChange={(e)=> { this.search(e)}}>
+                    {
+                        countries.map((country)=> {
+                            return <option value={country} key={country}>{country}</option>
+                        })
+                    }
+                </select>
+            )
+        }else{
+            return <div>no country</div>
+        }
+    }
+
     render() {
         return (
-            <div></div>
+            <div className="search_bar">
+                {this.renderSelectBox()}
+            </div>
         )
     }
 }
