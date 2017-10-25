a0279296da29fba25ccefe4d925b00a0797d5ab5
ergot <wjose14@hotmail.com>
2017-10-25 16:11:49 +0200

    4-43

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const ERROR_GET_COUNTRIES = "ERROR_GET_COUNTRIES"
 
 const API_END_POINT = 'http://api.population.io:80/1.0/'
 const DEFAULT_PARAM = '25/today'
-const GET_MORTALITY = 'GET_MORTALITY'
+export const GET_MORTALITY = 'GET_MORTALITY'
 
 
 export function getCountries(){
@@ -22,7 +22,7 @@ export function getMortality(country){
     return function (dispatch) {
         return axios(`${API_END_POINT}mortality-distribution/${country}/male/${DEFAULT_PARAM}`).then((responseMale)=> {
             axios(`${API_END_POINT}mortality-distribution/${country}/female/${DEFAULT_PARAM}`).then((responseFemale)=> {
-                console.log('yoloo')
+
                 dispatch(
                     {
                         type: GET_MORTALITY,
