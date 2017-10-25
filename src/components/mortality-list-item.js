14d8696a8b4e102b7164ac1b629a3281be9edd79
ergot <wjose14@hotmail.com>
2017-10-25 16:55:25 +0200

    4-45

--- src/components/mortality-list-item.js
+++ src/components/mortality-list-item.js
@@ -1,11 +1,17 @@
 import React from 'react'
 const URL_BASE = 'http://www.sciencekids.co.nz/images/pictures/flags680/'
 import Flag from './flag'
+import { ColumnChart } from 'react-chartkick';
+window.Chart = require('chart.js');
+const xTitle = "Age"
+const yTitle = "% mortalité"
+
 
 const MortalityListItem = () => {
     return (
         <div>
             <Flag country={"France"} className="flag_medium"/>
+            <ColumnChart data={[[12,45],[45,85]]} xtitle={xTitle} ytitle={yTitle}/>
         </div>
     )
 }
